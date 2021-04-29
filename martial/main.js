a9728051f1ee4816544b411e475f410c8561bf51
ramyapriya0025 <83345397+ramyapriya0025@users.noreply.github.com>
2021-04-29 07:39:51 +0530

    Update main.js

--- martial/main.js
+++ martial/main.js
@@ -6,7 +6,7 @@ let loadimage = (src, callback) => {
   img.src = src;
 };
 let imagepath = (imagenumber, animations) => {
-  return "/martial/"+ animations + "/" + String(imagenumber) + ".png";
+  return "martial-arts/blob/main/martial/"+ animations + "/" + String(imagenumber) + ".png";
 };
 
 let frames = {
