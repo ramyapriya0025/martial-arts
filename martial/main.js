cdc390037128786f6d339f9a93865d86acd749d2
ramyapriya0025 <83345397+ramyapriya0025@users.noreply.github.com>
2021-04-29 07:42:05 +0530

    Update main.js

--- martial/main.js
+++ martial/main.js
@@ -6,7 +6,8 @@ let loadimage = (src, callback) => {
   img.src = src;
 };
 let imagepath = (imagenumber, animations) => {
-  return "martial-arts/blob/main/martial/"+ animations + "/" + String(imagenumber) + ".png";
+  return "/martial-arts/martial/"+ animations + "/" + String(imagenumber) + ".png";
+  // /martial-arts/martial/idle/
 };
 
 let frames = {
